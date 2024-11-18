0e06692a2b6fa569d65f7042e5d2e89a6034f39f
Nic <nseymoursmith@gmail.com>
2024-11-18 21:29:49 +0000

    full mouse grid

--- conchord.py
+++ conchord.py
@@ -1,16 +1,16 @@
 import pygame
 import mido
+from math import floor
 
 # Initialize Pygame
 pygame.init()
 
 # Set up some constants
-WIDTH, HEIGHT = 640, 480
+WIDTH, HEIGHT = 1900, 850
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 CHORD_BUTTON_WIDTH = 100
 CHORD_BUTTON_HEIGHT = 50
-CHORD_BUTTON_SPACING = 10
 
 # Set up the display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -26,27 +26,38 @@ intervals = {
     "major": [0, 4, 3],
     "minor": [0, 3, 4],
     "dom7": [0, 4, 6],
-    "dim7": [-3, 0, 3],  # start from -3
+    "dim7": [-3, 0, 3],
 }
 
-root_notes = [25 + 7*i for i in range(12)]
-counter_bass = [i + 4 for i in root_notes]
+top_left = 100
+x_space = 150
+y_space = 150
 
-# Define the chords
-chords = {
-    "C": [60, 64, 67],  # C major
-    "G": [67, 71, 74],  # G major
-    "Am": [57, 60, 64],  # A minor
-    "F": [65, 69, 72],  # F major
-}
+rows = 5
+columns = 12
 
-# Define the chord buttons
-chord_buttons = [
-    {"text": "C", "x": 100, "y": 100},
-    {"text": "G", "x": 250, "y": 100},
-    {"text": "Am", "x": 400, "y": 100},
-    {"text": "F", "x": 100, "y": 250},
-]
+root_notes = [[25 + 7*i] for i in range(12)]
+root_names = ["Db", "Ab", "Eb", "Bb", "F", "C", "G", "D", "A", "E", "B", "F#"]
+counter_bass = [[i[0] + 4] for i in root_notes]
+cb_names = ["F", "C", "G", "D", "A", "E", "B", "F#", "C#", "G#", "D#", "A#"]
+major_notes = [list(map(lambda x: x + n[0] + 12, (0, 4, 7))) for n in root_notes]
+major_names = [f"{root_name}M" for root_name in root_names]
+minor_notes = [list(map(lambda x: x + n[0] + 12, (0, 3, 7))) for n in root_notes]
+minor_names = [f"{root_name}m" for root_name in root_names]
+seventh_notes = [list(map(lambda x: x + n[0] + 12, (-3, 0, 3))) for n in root_notes]
+seventh_names = [f"{root_name}7" for root_name in root_names]
+coordinates = [(top_left + (n % columns)*x_space,
+                top_left + floor(n / columns)*y_space)
+               for n in range(rows * columns)]
+
+notes = counter_bass + root_notes + major_notes + minor_notes + seventh_notes
+names = cb_names + root_names + major_names + minor_names + seventh_names
+
+chord_buttons = []
+for i in range(len(names)):
+    chord_buttons.append({"notes": notes[i],
+                          "text": names[i],
+                          "coords": coordinates[i]})
 
 # Game loop
 running = True
@@ -56,23 +67,24 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for button in chord_buttons:
-                if (button["x"] < event.pos[0] < button["x"] + CHORD_BUTTON_WIDTH and
-                        button["y"] < event.pos[1] < button["y"] + CHORD_BUTTON_HEIGHT):
+                if (button["coords"][0] < event.pos[0] < button["coords"][0] + CHORD_BUTTON_WIDTH and
+                        button["coords"][1] < event.pos[1] < button["coords"][1] + CHORD_BUTTON_HEIGHT):
+                    print(button)
                     # Play the chord
-                    for note in chords[button["text"]]:
+                    for note in button["notes"]:
                         msg = mido.Message('note_on', note=note, velocity=64)
                         output.send(msg)
                     pygame.time.wait(500)  # Wait for 0.5 seconds
-                    for note in chords[button["text"]]:
+                    for note in button["notes"]:
                         msg = mido.Message('note_off', note=note, velocity=64)
                         output.send(msg)
 
     # Draw everything
     screen.fill(BLACK)
     for button in chord_buttons:
-        pygame.draw.rect(screen, WHITE, (button["x"], button["y"], CHORD_BUTTON_WIDTH, CHORD_BUTTON_HEIGHT))
+        pygame.draw.rect(screen, WHITE, (button["coords"][0], button["coords"][1], CHORD_BUTTON_WIDTH, CHORD_BUTTON_HEIGHT))
         text_surface = font.render(button["text"], True, BLACK)
-        screen.blit(text_surface, (button["x"] + 10, button["y"] + 10))
+        screen.blit(text_surface, (button["coords"][0] + 10, button["coords"][1] + 10))
 
     pygame.display.flip()
 

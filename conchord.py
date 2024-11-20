09b972534fa66ede65539bfefe4a47b44b944e17
Nic <nseymoursmith@gmail.com>
2024-11-20 19:22:28 +0000

    keyboard but no mouse

--- conchord.py
+++ conchord.py
@@ -21,13 +21,13 @@ font = pygame.font.Font(None, 36)
 # Set up the MIDI output port
 output = mido.open_output()
 
-# Define chord intervals
-intervals = {
-    "major": [0, 4, 3],
-    "minor": [0, 3, 4],
-    "dom7": [0, 4, 6],
-    "dim7": [-3, 0, 3],
-}
+# Reference chord intervals (from root)
+#
+#     "major": [0, 4, 7],
+#     "minor": [0, 3, 7],
+#     "dom7": [0, 4, 10],
+#     "dim7": [-3, 0, 3],
+#
 
 top_left = 100
 x_space = 150
@@ -35,29 +35,66 @@ y_space = 150
 
 rows = 5
 columns = 12
+octave = -2  # From C3 bass (on 6th row)
 
-root_notes = [[25 + 7*i] for i in range(12)]
+# Db leftmost bass
+root_notes_lower = [[61 + 2 * i + octave * 12] for i in range(int(columns/2))]
+
+# Ab major fourth below
+root_notes_higher = [[56 + 2 * i + octave * 12] for i in range(int(columns/2))]
+
+root_notes = root_notes_lower + root_notes_higher
+# Interleave to get major fourth major fifth alternating
+root_notes[::2] = root_notes_lower
+root_notes[1::2] = root_notes_higher
 root_names = ["Db", "Ab", "Eb", "Bb", "F", "C", "G", "D", "A", "E", "B", "F#"]
-counter_bass = [[i[0] + 4] for i in root_notes]
+root_keys = [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4,
+             pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8,
+             pygame.K_9, pygame.K_0, pygame.K_MINUS, pygame.K_EQUALS]
+
+counter_bass = [[n[0] + 4] for n in root_notes]
 cb_names = ["F", "C", "G", "D", "A", "E", "B", "F#", "C#", "G#", "D#", "A#"]
-major_notes = [list(map(lambda x: x + n[0] + 12, (0, 4, 7))) for n in root_notes]
+cb_keys = [pygame.K_F1, pygame.K_F2, pygame.K_F3, pygame.K_F4,
+           pygame.K_F5, pygame.K_F6, pygame.K_F7, pygame.K_F8,
+           pygame.K_F9, pygame.K_F10, pygame.K_F11, pygame.K_F12]
+
+major_notes = [list(map(lambda x: x + n[0] + 12, (0, 4, 7)))
+               for n in root_notes]
 major_names = [f"{root_name}M" for root_name in root_names]
-minor_notes = [list(map(lambda x: x + n[0] + 12, (0, 3, 7))) for n in root_notes]
+major_keys = [pygame.K_q, pygame.K_w, pygame.K_e, pygame.K_r,
+              pygame.K_t, pygame.K_y, pygame.K_u, pygame.K_i,
+              pygame.K_o, pygame.K_p, pygame.K_LEFTBRACKET,
+              pygame.K_RIGHTBRACKET]
+
+minor_notes = [list(map(lambda x: x + n[0] + 12, (0, 3, 7)))
+               for n in root_notes]
 minor_names = [f"{root_name}m" for root_name in root_names]
-seventh_notes = [list(map(lambda x: x + n[0] + 12, (-3, 0, 3))) for n in root_notes]
+minor_keys = [pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_f,
+              pygame.K_g, pygame.K_h, pygame.K_j, pygame.K_k,
+              pygame.K_l, pygame.K_SEMICOLON, pygame.K_QUOTE, pygame.K_HASH]
+
+seventh_notes = [list(map(lambda x: x + n[0] + 12, (0, 4, 10)))
+                 for n in root_notes]
 seventh_names = [f"{root_name}7" for root_name in root_names]
+seventh_keys = [pygame.K_z, pygame.K_x, pygame.K_c, pygame.K_v,
+                pygame.K_b, pygame.K_n, pygame.K_m, pygame.K_COMMA,
+                pygame.K_PERIOD, pygame.K_SLASH, pygame.K_F15, pygame.K_F15]
+
+
 coordinates = [(top_left + (n % columns)*x_space,
                 top_left + floor(n / columns)*y_space)
                for n in range(rows * columns)]
 
 notes = counter_bass + root_notes + major_notes + minor_notes + seventh_notes
 names = cb_names + root_names + major_names + minor_names + seventh_names
+keys = cb_keys + root_keys + major_keys + minor_keys + seventh_keys
 
-chord_buttons = []
-for i in range(len(names)):
-    chord_buttons.append({"notes": notes[i],
-                          "text": names[i],
-                          "coords": coordinates[i]})
+chord_buttons = {}
+for i in range(len(keys)):
+    chord_buttons[keys[i]] = {"notes": notes[i],
+                              "text": names[i],
+                              "coords": coordinates[i],
+                              "keyboard": keys[i]}
 
 # Game loop
 running = True
@@ -65,23 +102,36 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            for button in chord_buttons:
-                if (button["coords"][0] < event.pos[0] < button["coords"][0] + CHORD_BUTTON_WIDTH and
-                        button["coords"][1] < event.pos[1] < button["coords"][1] + CHORD_BUTTON_HEIGHT):
-                    print(button)
-                    # Play the chord
-                    for note in button["notes"]:
-                        msg = mido.Message('note_on', note=note, velocity=64)
-                        output.send(msg)
-                    pygame.time.wait(500)  # Wait for 0.5 seconds
-                    for note in button["notes"]:
-                        msg = mido.Message('note_off', note=note, velocity=64)
-                        output.send(msg)
+        elif event.type == pygame.KEYDOWN:
+            if event.key in chord_buttons:
+                print(chord_buttons[event.key])
+                button = chord_buttons[event.key]
+                # Play the chord
+                for note in button["notes"]:
+                    msg = mido.Message('note_on', note=note, velocity=64)
+                    output.send(msg)
+                pygame.time.wait(500)  # Wait for 0.5 seconds
+                for note in button["notes"]:
+                    msg = mido.Message('note_off', note=note, velocity=64)
+                    output.send(msg)
+
+        # elif event.type == pygame.MOUSEBUTTONDOWN:
+        #     for button in chord_buttons:
+        #         if (button["coords"][0] < event.pos[0] < button["coords"][0] + CHORD_BUTTON_WIDTH and
+        #                 button["coords"][1] < event.pos[1] < button["coords"][1] + CHORD_BUTTON_HEIGHT):
+        #             print(button)
+        #             # Play the chord
+        #             for note in button["notes"]:
+        #                 msg = mido.Message('note_on', note=note, velocity=64)
+        #                 output.send(msg)
+        #             pygame.time.wait(500)  # Wait for 0.5 seconds
+        #             for note in button["notes"]:
+        #                 msg = mido.Message('note_off', note=note, velocity=64)
+        #                 output.send(msg)
 
     # Draw everything
     screen.fill(BLACK)
-    for button in chord_buttons:
+    for key, button in chord_buttons.items():
         pygame.draw.rect(screen, WHITE, (button["coords"][0], button["coords"][1], CHORD_BUTTON_WIDTH, CHORD_BUTTON_HEIGHT))
         text_surface = font.render(button["text"], True, BLACK)
         screen.blit(text_surface, (button["coords"][0] + 10, button["coords"][1] + 10))
